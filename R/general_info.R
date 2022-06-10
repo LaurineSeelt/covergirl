1db9a4450eee13b2254fa9667524196aa15b70c4
LaurineSeelt <laurine.seelt@student.hu.nl>
2022-06-10 10:28:12 +0200

    Functie general_info zo goed als af gemaakt?

--- R/general_info.R
+++ R/general_info.R
@@ -1,3 +1,15 @@
+#' Gain some general information about contestants on RuPaul's Drag Race
+#'
+#' @param character A name of a contestant of RuPaul's Drag Race
+#'
+#' @return A character vector
+#' @export
+#'
+#' @examples
+#' library(tidyverse)
+#' library(dragracer)
+#'
+#' general_info("Jinkx Monsoon")
 general_info <- function(character){
   general <- rpdr_contestants %>% filter(contestant == character) %>%
     select(season, age, hometown) %>% unlist()
