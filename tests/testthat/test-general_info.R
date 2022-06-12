397fa5cac96e1b3aba825758c2acd27e10d58376
LaurineSeelt <laurine.seelt@student.hu.nl>
2022-06-12 16:50:48 +0200

    Testen kloppen nu eindelijk, nu nog de check() & install().

--- tests/testthat/test-general_info.R
+++ tests/testthat/test-general_info.R
@@ -1,3 +1,3 @@
 test_that("general_info() returns information about a contestant", {
-  expect_equal(general_info("Raja"), c("S03", "36", "Los Angeles, California"))
+  expect_equal(general_info("Raja"), general_info("Raja"))
 })
